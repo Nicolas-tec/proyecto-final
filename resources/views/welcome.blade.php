522e91b65f2dd3a67c75d99417fdd338af950859
Nicolas-tec <71329970+Nicolas-tec@users.noreply.github.com>
2024-04-01 18:05:44 -0500

    ajustes-2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/d73986632c.js" crossorigin="anonymous"></script>
 <style>
         body {
             background: silver;
